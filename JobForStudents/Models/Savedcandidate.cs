6d7d776806dc1fc2965af0b7af0672892ce4b804
Le Anh Tuan <tuanlace180905@fpt.edu.vn>
2026-06-26 00:09:13 +0700

    hotfix Savadcandiate

--- JobForStudents/Models/Savedcandidate.cs
+++ JobForStudents/Models/Savedcandidate.cs
@@ -1,15 +1,13 @@
-﻿using System;
+using System;
 
 namespace JobForStudents.Models;
 
 public class SavedCandidate
 {
-    public int Id { get; set; }
     public int BusinessId { get; set; }
     public int StudentId { get; set; }
     public DateTime SavedAt { get; set; } = DateTime.UtcNow;
 
-    // Navigation properties
     public virtual BusinessProfile BusinessProfile { get; set; } = null!;
     public virtual StudentProfile StudentProfile { get; set; } = null!;
 }
